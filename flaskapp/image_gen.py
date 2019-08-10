ca39d7549453beb28b9480b85548bea79033155d
root <root@LAPTOP-UR3SONCA.localdomain>
2019-08-09 22:02:58 -0400

    idk, I am having an error image_gen not a module

--- flaskapp/image_gen.py
+++ flaskapp/image_gen.py
@@ -5,5 +5,7 @@ def generate_url():
                 "https://images.pexels.com/photos/583848/pexels-photo-583848.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
                 "https://images.pexels.com/photos/1054218/pexels-photo-1054218.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
                 "https://images.pexels.com/photos/949587/pexels-photo-949587.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-                "https://images.pexels.com/photos/1323550/pexels-photo-1323550.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"]
+                "https://images.pexels.com/photos/1323550/pexels-photo-1323550.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+                "https://images.pexels.com/photos/1629236/pexels-photo-1629236.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940", 
+                "https://images.pexels.com/photos/531756/pexels-photo-531756.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"]
     return url_list[random.randint(1, len(url_list)-1)]
